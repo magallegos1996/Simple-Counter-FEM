9149d367fe361dbf92bc91f4ef06a44c42aa5214
magallegos1996 <bwcekos@gmail.com>
2020-10-26 11:30:17 -0500

    Refactoring to Hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,53 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 3,
-    }
-    /*
-    Tienes que tener mucho cuidado en cuanto al significado de 'this' en los callbacks de JSX.
-    En JavaScript, los métodos de clase no están ligados por defecto. Si olvidas ligar this.increment (o decrement o reset)
-    y lo pasas a onClick, 'this' será undefined cuando se llame la función.*/
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-    this.reset = this.reset.bind(this);
+const Counter = ({max}) => {
+  const [count, setCount] = React.useState(0);
+  const increment = () => {
+    setCount(c => {
+      if (c >= max) return c;
+      return c + 1;
+    });
   }
-  storeStateInLocalStorage() {
-    localStorage.setItem('counterState', JSON.stringify(this.state));
-  }
-  increment() {
-    this.setState(increment, this.storeStateInLocalStorage);
-  }
-  decrement() {
-    this.setState({count: this.state.count - 1});
-  }
-  reset() {
-    this.setState({count: 0})
-  }
-  render() {
-    const { count } = this.state;
-    return (
-      <div className="Counter">
-        <p className="count">{ count }</p>
-        <section className="controls">
-          <button onClick={this.increment}>Increment</button>
-          <button onClick={this.decrement}>Decrement</button>
-          <button onClick={this.reset}>Reset</button>
-        </section>
-      </div>
-    );
-  }
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  return (
+    <main className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </main>
+  );
+
 }
-const increment = (state, props) => {
-  if (state.count >= props.max) return;
-  return { count: state.count + 1 };
-};
-const getStateFromLocalStorage = () => {
-  const storage = localStorage.getItem('counterState');
-  if (storage) return JSON.parse(storage);
-  return { count: 0 };
-};
 
 export default Counter;
